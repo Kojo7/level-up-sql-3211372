52499991b2594db6d434a8c772377d71630a7fdb
Nana-Kena <42452199+Kojo7@users.noreply.github.com>
2023-01-02 15:43:14 +0000

    Finished the last challenge

--- 02_library/challenges/02_08_bookstats_ch.sql
+++ 02_library/challenges/02_08_bookstats_ch.sql
@@ -5,3 +5,15 @@
 
 -- Report 2: Show the five books that have been
 -- checked out the most.
+
+SELECT Published, COUNT(DISTINCT(Title)) as PublishedCount, GROUP_CONCAT(DISTINCT(Title)) as Title
+FROM BOOKS
+GROUP BY Published
+ORDER BY PublishedCount DESC;
+
+SELECT COUNT(Loans.LoanID) AS TopFive, Books.Title, Books.Author, Books.Published
+FROM Loans
+JOIN Books ON Loans.BookID = Books.BookID
+GROUP BY Books.Title
+ORDER BY TopFive DESC
+LIMIT 5;
